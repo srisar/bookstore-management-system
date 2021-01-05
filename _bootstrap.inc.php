37fe876ac1c838f957c28dc5302267610ade7e89
Srisaravana Manicaraja <saravanamuthaly@gmail.com>
2021-01-05 10:34:27 +0530

    updated dir structure for better isolation of source files

--- _bootstrap.inc.php
+++ _bootstrap.inc.php
@@ -31,7 +31,7 @@ Database::init($db_config);
  */
 
 define('SITE_URL', 'http://localhost');
-define('BASE_PATH', __DIR__);
+define('BASE_PATH', __DIR__ . '/public');
 define('APP_NAME', $_ENV['APP_NAME']);
 define('APP_VERSION', $_ENV['APP_VERSION']);
 
